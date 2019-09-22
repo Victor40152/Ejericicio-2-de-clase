5b6df478013fd596da50a44424d0e39d80a8501e
Victor Manuel Echavarria <vicme93@gmail.com>
2019-09-22 12:09:47 -0500

    Ejercicio 2 programacion de .net

---
+++ conversiondegrados/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace conversiondegrados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtgrados = new System.Windows.Forms.TextBox();
+            this.btnconvertir = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculo de Grados Centigrados a Farenheit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(292, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite los grados Centigrados:";
+            // 
+            // txtgrados
+            // 
+            this.txtgrados.Location = new System.Drawing.Point(394, 144);
+            this.txtgrados.Name = "txtgrados";
+            this.txtgrados.Size = new System.Drawing.Size(100, 20);
+            this.txtgrados.TabIndex = 2;
+            // 
+            // btnconvertir
+            // 
+            this.btnconvertir.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnconvertir.Location = new System.Drawing.Point(584, 140);
+            this.btnconvertir.Name = "btnconvertir";
+            this.btnconvertir.Size = new System.Drawing.Size(105, 33);
+            this.btnconvertir.TabIndex = 3;
+            this.btnconvertir.Text = "Convertir";
+            this.btnconvertir.UseVisualStyleBackColor = true;
+            this.btnconvertir.Click += new System.EventHandler(this.btnconvertir_Click);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(72, 250);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(104, 24);
+            this.lblresultado.TabIndex = 4;
+            this.lblresultado.Text = "Resultado";
+            this.lblresultado.Visible = false;
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.btnconvertir);
+            this.Controls.Add(this.txtgrados);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtgrados;
+        private System.Windows.Forms.Button btnconvertir;
+        private System.Windows.Forms.Label lblresultado;
+    }
+}
+
---
+++ conversiondegrados/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conversiondegrados
+{
+    public partial class Form1 : Form
+    {
+
+        Grados grados = new Grados();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnconvertir_Click(object sender, EventArgs e)
+        {
+            grados.setNumero1(double.Parse(txtgrados.Text));
+            {
+                lblresultado.Text = "Resultado es:" + grados.convertir().ToString();
+                lblresultado.Visible = true;
+
+            }
+        }
+
+        
+        
+    }
+}
